9a82db190bbc19f1e72b3f3e409af61f67da0fc1
tjclutter <tjclutter@outlook.com>
2023-08-12 10:53:27 -0400

    Comments

--- CT_Module_5_option1.java
+++ CT_Module_5_option1.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
 
 public class CT_Module_5_option1 {
+	//header of recursive method MultiplyNum
 	public static int MultiplyNum(int numLeft, int sum) {
+		//create scanner object named scnr
 		Scanner scnr = new Scanner(System.in);
+		//check if numLeft is equal to zero
+		//if so return sum
 		if (numLeft == 0) {
 			return sum;
+		//if numLeft does not equal zero:
 		}else {
+			//prompt user to enter a number
+			//and store it in num
 			System.out.println("Enter number: ");
 			int num = scnr.nextInt();
+			//calculate new sum using new number num
+			//and the value of the old sum
 			sum = num * sum;
+			//call recursive method MultiplyNum
+			//with updated sum and one less than numLeft
+			//store output in sum
 			sum = MultiplyNum(numLeft - 1, sum);
 		}
+		//return sum
 		return sum;
 	}
 	public static void main(String[] args) {
+		//call recursive method with numLeft equaling five for five numbers
+		//and sum equaling one. store output in variable product 
 		int product = MultiplyNum(5, 1);
+		//print product which stores product of the five numbers entered by user
 		System.out.println("The product of your five numbers is: " + product);
 	}
 }
